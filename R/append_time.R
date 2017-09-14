7f9b27dccf554a36f06a31e5a195624d53a9d1bd
talegari <sri.teach@gmail.com>
2017-09-14 13:48:51 +0530

    added 'ext' argument to 'append_time'

--- R/append_time.R
+++ R/append_time.R
@@ -2,32 +2,29 @@
 #' @description format:
 #'   filename__year-month-day_hour-minutes-seconds_timezone.extension
 #' @param path (string) file path (existence is not checked)
+#' @param ext (string) Optional extension for the file
 #' @return (string) A file path with time stamp appended in the format described
 #'   in the description
 #' @examples
 #' append_time("hello/a.b")
+#' append_time("hello/a.b", "R")
 #' @export
 
-append_time = function(path){
+append_time <- function(path, ext){
 
   assert_that(is.string(path))
-
-  ext        = tools::file_ext(path)
-  withoutExt = tools::file_path_sans_ext(path)
-
-  withTime   = paste(withoutExt
-                     , gsub(":", "-", gsub(" ", "_", Sys.time()))
-                     , sep = "__"
-                     )
-  withTime   = paste(withTime
-                     , gsub("/", "-", Sys.timezone())
-                     , sep = "_"
-                     )
-
-  if(nchar(ext) > 0){
-    withTime   = paste(withTime, ext, sep = ".")
+  if(!missing(ext)){
+    assert_that(is.string(ext))
+  } else {
+    ext = tools::file_ext(path)
   }
-
+  withoutExt = tools::file_path_sans_ext(path)
+  withTime   = paste(withoutExt
+                     , gsub(":", "-", gsub(" ", "_", Sys.time())), sep = "__"
+                     )
+  withTime   = paste(withTime, gsub("/", "-", Sys.timezone()), sep = "_")
+  if (nchar(ext) > 0) {
+    withTime = paste(withTime, ext, sep = ".")
+  }
   return(withTime)
-
 }
